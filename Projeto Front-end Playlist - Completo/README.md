e5d35c9c4b82971120003f813584361823a78b85
Wictor Huggo Marques <92128904+Huggomarqs@users.noreply.github.com>
2022-10-19 13:49:02 -0300

    Projeto front-end Playlist - Completo

---
+++ Projeto Front-end Playlist - Completo/README.md
@@ -0,0 +1,11 @@
+# PROA-projects/Playlist
+Projetos realizados no Proa PROPROFISSÃO
+
+Gabriel Augusto, professor do PROA pediu uma playlist com seus gostos musicais. Pediu um wireframe, um protótipo e o site em si, e foi o que fiz.
+
+Inicialmente projetei o design das telas no figma, somente os wireframes, utilizando algumas inspirações. 
+
+logo após isso eu fui criar o protótipo, tela por tela no figma, acabei encontrando alguns desafios, mas foi uma experiência deliciosa, adorei.
+
+A parte mais complicada foi transformar em site, utilizando HTML, CSS e JS, quebrei a cabeça, mas graças aos meus conhecimentos adquiridos, consegui concluir com exito.
+Não foi exatamente como eu queria, algumas funções faltaram(95% do planejado foi implementado), porém estou orgulhoso, muito obrigado instituto PROA pela oportunidade.
---
+++ Projeto Front-end Playlist - Completo/playlist/musiclist.js
@@ -0,0 +1,104 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    letra: "Assim, que o dia amanheceu, lá no mar alto da paixão, dava pra ver o tempo ruir. Cadê você? Que solidão! Esquecera de mim? Enfim, de tudo o que há na terra, não há nada em lugar nenhum, que vá crescer sem você chegar. Longe de ti tudo parou, ninguém sabe o que eu sofri. Amar é um deserto e seus temores, vida que vai na sela dessas dores, não sabe voltar, me dá teu calor. Vem me fazer feliz porque eu te amo, você desagua em mim, e eu, oceano, e esqueço que amar é quase uma dor. Só sei viver se for por você. Assim, que o dia amanheceu, lá no mar alto da paixão, dava pra ver o tempo ruir. Cadê você? Que solidão! Esquecera de mim? Enfim, de tudo o que há na terra, não há nada em lugar nenhum, que vá crescer sem você chegar. Longe de ti tudo parou, ninguém sabe o que eu sofri. Amar é um deserto e seus temores, vida que vai na sela dessas dores, não sabe voltar, me dá teu calor. Vem me fazer feliz porque eu te amo, você desagua em mim, e eu, oceano, e esqueço que amar é quase uma dor. Só sei viver se for por você.",
+    name: "Oceano",
+    artist: "Djavan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    letra: "Some people think they're always right, others are quiet and uptight, others, they seem so very nice-nice-nice-nice, oh, inside they might feel sad and wrong, oh, no. Twenty-nine different attributes, only seven that you like, oh-oh twenty ways to see the world, oh, and twenty ways to start a fight, oh. Oh don't, don't, don't get up, I can't see the sunshine, I'll be waiting for you, baby, 'cause I'm through, sit me down, shut me up, I'll calm down, and I'll get along with you. Oh, men don't notice what they got, oh, women think of that a lot, one thousand ways to please your man, oh, not even one requires a plan, I know. And countless odd religions too, it doesn't matter which you choose, oh, no, one stubborn way to turn your back, oh, this I've tried and now refuse, oh. Oh don't, don't, don't get up, I can't see the sunshine, oh, I'll be waiting for you, baby 'cause I'm through, sit me down, shut me up, I'll calm down, and I'll get along with you. Alright. Shut me up, shut me up. And I'll get along with you.",
+    name: "You Only Live Once",
+    artist: "The Strokes",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    letra: "Yesterday, all my troubles seemed so far away, now it looks as though they're here to stay, oh, I believe in yesterday. Suddenly, I'm not half the man I used to be, there's a shadow hanging over me, oh, yesterday came suddenly. Why she had to go, I don't know, she wouldn't say, I said something wrong, now I long for yesterday. Yesterday, love was such an easy game to play, now I need a place to hide away, oh, I believe in yesterday. Why she had to go, I don't know, she wouldn't say, I said something wrong, now I long for yesterday. Yesterday, love was such an easy game to play, now I need a place to hide away, oh, I believe in yesterday, mm, mm, mm, mm, mm, mm, mm.",
+    name: "Yesterday",
+    artist: "The Beatles",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    letra: "You walked away like silent rain that gives place to a sunny day. I rest my head, knowing that some day I'll understand the pain. Thoughts will come and ease my life, peace of mind is what I shall find, don't expect for me to cry for you. And when you think of calling me, remember I won't answer 'cause I'm busy licking my wounds, in the deepest cave, I could find inside me. We used to say that everything should end if it's for the best, I found a way to look at things as something merely from the past, our love was so intense, that even now we still can't be friends, sad, but that's the way it's gonna be. And when you think of calling me, remember I won't answer 'cause I'm busy licking my wounds, in the deepest cave, I could find inside me.",
+    name: "Deepest Cave",
+    artist: "Tiago Carvalho",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    letra: "Bate outra vez, com esperanças o meu coração, pois já vai terminando o verão, enfim. Volto ao jardim, com a certeza que devo chorar, pois bem sei que não queres voltar, para mim. Queixo-me às rosas, que bobagem as rosas não falam, simplesmente as rosas exalam, o perfume que roubam de ti, ai. Devias vir, para ver os meus olhos tristonhos, e quem sabe, sonhavas meus sonhos, por fim. Bate outra vez, com esperanças o meu coração, pois já vai terminando o verão, enfim. Volto ao jardim, com a certeza que devo chorar, pois bem sei que não queres voltar, para mim. Queixo-me às rosas, que bobagem as rosas não falam, simplesmente as rosas exalam, o perfume que roubam de ti, ai. Devias vir, para ver os meus olhos tristonhos, e quem sabe, sonhavas meus sonhos, por fim. Devias vir, para ver os meus olhos tristonhos, e quem sabe, sonhavas meus sonhos, por fim.",
+    name: "As Rosas Não Falam",
+    artist: "Cartola",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    letra: "Eu sinto o vento no meu rosto, e a estrada atrás de mim, vai desenhando a nossa história até o fim. Não há caminho certo pra gente seguir, essa certeza é o que a gente decidir, porque o destino final não é o que importa. Milhões de estrelas nos olhando lá do céu, e nessa vida eu vou buscando o meu papel, porque a sorte sorri se eu sorrir de volta. Alguns lugares mais escuros percorri, e admito que alguns erros cometi, mas jamais vão dizer que eu não vivi. Quantos dias eu passei aqui uoooo, sem saber pra onde devo ir uoooo, Quero saber tudo que há, eu não vou mais me importar, quantas noites eu passei aqui. A paz e o amor na brisa de uma canção, e eu só queria segurar a tua a mão, as flores no teu cabelo me dão esperança. Eu sinto o vento no meu rosto, e a estrada atrás de mim, vai desenhando a nossa história até o fim. De pés descalços vou andando, até o rumo aparecer, com a promessa de um novo amanhecer. Quantos dias eu passei aqui uoooo, sem saber pra onde devo ir uoooo, quero saber tudo que há, eu não vou mais me importar, quantas noites eu passei aqui.",
+    name: "Estelar",
+    artist: "Pouring Pix",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    letra: "Something inside the cards I know is right, don't want to live somebody else's life, this is what I want to be and this is what I give to you, because I get it free, she smiles while I do my time. I could die for you, oh, this life I choose. I'm here to be your only go-between, to tell you of the sights these eyes have seen, what I really want to do is turn it into motion, beauty that I can't abuse, you know that I use my senses, too, you can see that it's only everywhere, I'd take it all in, then I'd find a way to share. Come along and go along with me, wander with me, yo, it's all for free. I could die for you, what you wanna do? Oh, this life I choose(2x). Come again and tell me where you want to go, what it means to me to be with you alone, close the door and no one has to know, how we are. Come along and go along with me, wander with me, yo, it's all for free. I could die for you, what you wanna do? Oh, this life I choose, I could die for you, what you wanna do? Oh, this life I choose, make me wanna say, I could die for you, what you wanna do? Oh, this life I choose(2x).",
+    name: "I Could Die for You",
+    artist: "Red Hot Chili Peppers",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    letra: "My 16 locked and loaded, all fear has been avoided, you say the words and my weapon is drawn. This one could be my last time, some people call it war crime, I may be staring down a lethal sight. Nothing shocks you like a bullet hole, leaving my fear on the danger line, suffering no man should ever know, leaving my faith on the danger line. I do this for my family, my daughter loves her daddy, too many talk down on things they don't know. With colors never faded, reckless and unabated, they may take me but never take us all. Nothing shocks you like a bullet hole, leaving my fear on the danger line, suffering no man should ever know, leaving my faith on the danger line. I know what you're thinking, I've been there before. So think of the times, the time we spent laughing away, so think of the times at home. Now I find myself in my own blood, the damage done is far beyond repair, I never put my faith in up above, but now, I'm hoping someone's there. I never meant to leave this world alone, I never meant to hurt the ones who care, and all this time I thought we'd just grow old, you know, no one said it's fair. Tell my baby girl that it's alright, I've sung my last song today, remind the Lord to leave his light on for me, I'm free.",
+    name: "Danger Line",
+    artist: "Avenged Sevenfold",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    letra: "You take a mortal man, and put him in control, watch him become a God, watch people's heads a-roll. A-roll, a-roll. Just like the pied piper, led rats through the streets, we dance like marionettes, swaying to the symphony of destruction. Acting like a robot, it's metal brain corrodes, you try to take its pulse, before the head explodes. Explodes, explodes, argh. Just like the pied piper, led rats through the streets, we dance like marionettes, swaying to the symphony. Just like the pied piper, led rats through the streets, we dance like marionettes, swaying to the symphony, swaying to the symphony of destruction. The Earth starts to rumble, world powers fall, a warring for the heavens, a peaceful man stands tall. A-tall, a-tall. Just like the pied piper, led rats through the streets, we dance like marionettes, swaying to the symphony. Just like the pied piper, led rats through the streets, we dance like marionettes, swaying to the symphony, swaying to the symphony of destruction.",
+    name: "Symphony Of Destruction",
+    artist: "Megadeth",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    letra: "Can the dark side light my way out? Yeah, lay your hand across my face, yeah, time we lost, that's all my fault. Please don't be long, 'cause I want your love, I don't have love, without your arm, life is too short, but I will live for you. How did this fit in your story? Yeah, why'd you let them judge your body? Yeah, I'll be waitin' there outside, yeah. Please don't be long, 'cause I want your arms, I don't have fun, without your love, life is too short, but I will live for you, you're backin' off, but I will live for you, my selfless love. Bite my tongue, I wait my turn, I waited for a century, waste my breath, no lessons learned, I turn and face the enemy, I'm not scared, just don't care, I'm not listenin', you hear? He's never there, I don't care, I can't tell you what and where, yeah, yeah.",
+    name: "Selfless",
+    artist: "The Strokes",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    letra: "Hoje acordei sem lembrar, se vivi ou se sonhei, você aqui nesse lugar, que eu ainda não deixei. Vou ficar? Quanto tempo vou esperar, e eu não sei o que vou fazer não. Nem precisei revelar, sua foto não tirei, como tirei pra dançar, alguém que avistei. Tempo atrás, esse tempo está lá trás, e eu não tenho mais o que fazer, não. Eu ainda gosto dela, mas ela já não gosta tanto assim, a porta ainda está aberta, mas da janela já não entra luz. E eu ainda penso nela, mas ela já não pensa mais em mim, em mim não. Ainda vejo o luar, refletido na areia, aqui na frente desse mar, sua boca eu beijei. Vou ficar, só com ela eu quis ficar, e agora ela me deixou oh. Eu ainda gosto dela, mas ela já não gosta tanto assim, a porta ainda está aberta, mas da janela já não entra luz(2x). E eu ainda penso nela, mas ela já não pensa mais em mim, eu vou deixar a porta aberta, pra que ela entre e traga a sua luz(2x).",
+    name: "Ainda Gosto Dela",
+    artist: "Skank",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    letra: "Ela é amiga da minha mulher, pois é, pois é, mas vive dando em cima de mim, enfim, enfim, ainda por cima, é uma tremenda gata, pra piorar a minha situação. Se fosse mulher feia, tava tudo certo, mulher bonita mexe com meu coração, se fosse mulher feia, tava tudo certo, mulher bonita mexe com meu coração. Não pego, eu pego(2x), não pego, não, não pego, eu pego(2x), não pego, não. Minha mulher me perguntou até: 'Qual é, qual é?', eu respondi que não tô nem aí, menti, menti, de vez em quando, eu fico admirando:'É muita areia pro meu caminhão', se fosse mulher feia, tava tudo certo, mulher bonita mexe com meu coração, se fosse mulher feia, tava tudo certo, mulher bonita mexe com meu coração. Não pego, eu pego(2x), não pego, não, não pego, eu pego(2x), não pego, não. O meu cunhado já me avisou, que se eu der mole, ele vai me entregar, a minha sogra me orientou:'Isso não tá certo, é melhor parar'. Falei, ela não quis ouvir, pedi, ela não respeitou, eu juro, a carne é fraca, mas nunca rolou(2x).",
+    name: "Amiga da Minha Mulher",
+    artist: "Seu Jorge",
+    img: "music-12",
+    src: "music-12"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
